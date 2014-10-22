38ad5afb57fae31252dc506ab2e92c1c1f175a92
AndrewWUw <andrew_wu@outlook.com>
2014-10-22 21:30:50 +1100

    worknig on header definitions

--- ass4/btree_iterator.h
+++ ass4/btree_iterator.h
@@ -2,6 +2,7 @@
 #define BTREE_ITERATOR_H
 
 #include <iterator>
+using namespace std;
 
 /**
  * You MUST implement the btree iterators as (an) external class(es) in this file.
@@ -10,6 +11,34 @@
 
 // iterator related interface stuff here; would be nice if you called your
 // iterator class btree_iterator (and possibly const_btree_iterator)
+template<typename T> class btree;
+template<typename T> class btree_iterator;
+template<typename T> class const_btree_iterator;
+
+template<typename T> class btree_iterator {
+	typedef ptrdiff_t                  	difference_type;
+	typedef bidirectional_iterator_tag	iterator_category;
+	typedef T 						   	value_type;
+	typedef T* 							pointer;
+	typedef T& 							reference;
+	friend class const_btree_iterator<T>;
+
+	reference operator*() const;
+	pointer operator->() const { return &(operator*()); }
+	btree_iterator& operator++();
+	bool operator==(const List_Iterator& other) const;
+	bool operator!=(const List_Iterator& other) const
+	{ return !operator==(other); }
+	List_Iterator(typename List<T>::Node *pointee = nullptr) : pointee_(pointee) {}
+	private:
+	typename List<T>::Node *pointee_;
+
+
+};
+
+template<typename T> class const_btree_iterator {
+
+};
 
 #include "btree_iterator.tem"
 
